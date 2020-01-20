3392d497fcd33040b69c2136ee97d39d5bdbeb09
Xiutao Zhang <Xiutao.Zhang@dfki.de>
2020-01-20 09:46:14 +0100

    windows version bin added and dataset

--- bin/test-object-detector.py
+++ bin/test-object-detector.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import os
 import shutil
+import platform
 # Link to the UIUC Car Database
 # http://l2r.cs.uiuc.edu/~cogcomp/Data/Car/CarData.tar.gz
 # dataset_url = "http://l2r.cs.uiuc.edu/~cogcomp/Data/Car/CarData.tar.gz"
@@ -11,19 +12,38 @@ import shutil
 #     os.system("wget {} -O {}".format(dataset_url, dataset_path))
 #     os.system("tar -xvzf {} -C {}".format(dataset_path, os.path.split(dataset_path)[0]))
 
-mod = "1"
+mod = "T"
 
-if mod == "Train":
-    pos_path = "../data/dataset/BaustelleData/TrainingImages/pos"
-    neg_path = "../data/dataset/BaustelleData/TrainingImages/neg"
-    pos_feat_path =  "../data/features/pos"
-    neg_feat_path =  "../data/features/neg"
-    shutil.rmtree(pos_feat_path)
-    shutil.rmtree(neg_feat_path)
-    os.mkdir(pos_feat_path)
-    os.mkdir(neg_feat_path)
-    os.system("python ../object-detector/extract-features.py -p {} -n {}".format(pos_path, neg_path))
-    os.system("python ../object-detector/train-classifier.py -p {} -n {}".format(pos_feat_path, neg_feat_path))
-else:
-    test_im_path = "../data/dataset/BaustelleData/TestImages/baustelle_10.jpg"
-    os.system("python ../object-detector/test-classifier.py -i {} -d {}".format(test_im_path,3))
+if platform.system() == "Linux":
+    print("Linux")
+    if mod == "Train":
+        pos_path = "../data/dataset/BaustelleData/TrainingImages/pos"
+        neg_path = "../data/dataset/BaustelleData/TrainingImages/neg"
+        pos_feat_path =  "../data/features/pos"
+        neg_feat_path =  "../data/features/neg"
+        shutil.rmtree(pos_feat_path)
+        shutil.rmtree(neg_feat_path)
+        os.mkdir(pos_feat_path)
+        os.mkdir(neg_feat_path)
+        os.system("python ../object-detector/extract-features.py -p {} -n {}".format(pos_path, neg_path))
+        os.system("python ../object-detector/train-classifier.py -p {} -n {}".format(pos_feat_path, neg_feat_path))
+    else:
+        test_im_path = "../data/dataset/BaustelleData/TestImages/baustelle_10.jpg"
+        os.system("python ../object-detector/test-classifier.py -i {} -d {}".format(test_im_path,3))
+
+elif platform.system() == "Windows":
+    print("Windows")
+    if mod == "Train":
+        pos_path = "..\\..\\data\\dataset\\BaustelleData\\TrainingImages\\pos"
+        neg_path = "..\\..\\data\\dataset\\BaustelleData\\TrainingImages\\neg"
+        pos_feat_path =  "..\\..\\data\\features\\pos"
+        neg_feat_path =  "..\\..\\data\\features\\neg"
+        shutil.rmtree(pos_feat_path)
+        shutil.rmtree(neg_feat_path)
+        os.mkdir(pos_feat_path)
+        os.mkdir(neg_feat_path)
+        os.system("python ..\\object-detector\\extract-features.py -p {} -n {} -pf {} -nf {}".format(pos_path, neg_path, pos_feat_path, neg_feat_path))
+        os.system("python ..\\object-detector\\train-classifier.py -p {} -n {}".format(pos_feat_path, neg_feat_path))
+    else:
+        test_im_path = "..\\..\\data\\dataset\\BaustelleData\\TestImages\\baustelle3.jpg"
+        os.system("python ..\\object-detector\\test-classifier.py -i {} -d {}".format(test_im_path,3))
